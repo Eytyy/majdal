aaff0b0ee1c00222122294fc7d28fb273ce2a153
Eytyy <e.tayyem@gmail.com>
2016-10-07 18:54:46 +0300

    scroll

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -67,7 +67,7 @@ class Home extends React.Component {
       },
     };
     return (
-      <Hammer onSwipe={this.handleSwipe} direction={true} vertical={true}>
+      <Hammer onSwipe={this.handleSwipe} direction={'vertical'} >
         <section className="section-home-wrapper">
           <Video active={this.state.active} />
           <Oils />
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -27224,7 +27224,7 @@
 	      };
 	      return _react2.default.createElement(
 	        _reactHammerjs2.default,
-	        { onSwipe: this.handleSwipe, direction: true, vertical: true },
+	        { onSwipe: this.handleSwipe, direction: 'vertical' },
 	        _react2.default.createElement(
 	          'section',
 	          { className: 'section-home-wrapper' },
